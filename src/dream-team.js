5bc4c094e35918075745ecf8535dcf22f52cafc7
abadire <panvan97@gmail.com>
2020-09-29 12:07:56 +0300

    feat: add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members)
+{
+  if (!Array.isArray(members)) return false;
+  return members.filter(x => typeof x === 'string')
+                .map(x => x.trim().toUpperCase())
+                .sort()
+                .reduce((acc, curr) => acc += curr[0], '');
 };
+
+module.exports = createDreamTeam;
+
+// console.log(createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]));
